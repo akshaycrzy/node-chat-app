0a1151b74f3106911b8f79fe9c3c0d1ac9b0eb42
akshay <ak016649@gmail.com>
2017-10-02 13:01:58 +0530

    Format timestamps using moment.js

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -10,8 +10,24 @@ describe('generateMessage', () => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({
-      form,
+      from,
       text
+    });
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate current location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 53;
+    var url = 'https://www.google.com/maps?q=15,53';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({
+      from,
+      url
     });
   });
 });
