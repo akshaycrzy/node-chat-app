5cf00087fa08e5ee333530d10dca63042ba72b57
akshay <ak016649@gmail.com>
2017-10-02 13:10:26 +0530

    Format timestamps using moment.js

--- public/js/app.js
+++ public/js/app.js
@@ -9,21 +9,50 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   console.log('New Message', message);
 
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_black">My Current Location</a>');
+
+  li.text(`${message.from}`);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#message').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
+    messageTextbox.val('');
+  });
+});
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation) {
+    alert("Your browser doesn't supported geolocation");
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, function() {
+      alert('Unable to fetch your location');
+    });
   });
 });
